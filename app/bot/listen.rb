53276d05be1a7e709df42625eee4a4a1ff96c480
seucroch <pablo.seuc.rocher@gmail.com>
2017-02-10 13:19:46 -0800

    fb messenger chat bot first commit

---
+++ app/bot/listen.rb
@@ -0,0 +1,7 @@
+# app/bot/listen.rb
+require "facebook/messenger"
+include Facebook::Messenger
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+Bot.on :message do |message|
+  puts "got your message!"
+end
